82a0fa90fd3f638ace91a1c13ea373b13f480fe3
Rishabh <rishabhroy186@gmail.com>
2024-12-07 18:21:22 +0530

    updated_nltk downloaders

--- download_nltk_data.py
+++ download_nltk_data.py
@@ -1,6 +1,17 @@
+import os
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('punkt_tab')
+
+# Define the path to store NLTK data
+nltk_data_path = '/app/nltk_data'
+
+# Download necessary NLTK data if not already present
+if not os.path.exists(os.path.join(nltk_data_path, 'tokenizers/punkt')):
+    nltk.download('punkt', download_dir=nltk_data_path)
+if not os.path.exists(os.path.join(nltk_data_path, 'corpora/stopwords')):
+    nltk.download('stopwords', download_dir=nltk_data_path)
+if not os.path.exists(os.path.join(nltk_data_path, 'corpora/wordnet')):
+    nltk.download('wordnet', download_dir=nltk_data_path)
+if not os.path.exists(os.path.join(nltk_data_path, 'corpora/omw-1.4')):
+    nltk.download('omw-1.4', download_dir=nltk_data_path)
+
+print("NLTK data downloaded successfully.")
